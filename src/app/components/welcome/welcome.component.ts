e20a2aa22b091ff9cefa7e02d871dbac298a6edf
poorna99122 <46306217+poorna99122@users.noreply.github.com>
2024-11-18 18:36:56 +0530

    added few style changes

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -11,8 +11,7 @@ export class WelcomeComponent implements OnInit{
   constructor(private router : Router){
 
   }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  ngOnInit() {
   }
 
   getStarted(){
